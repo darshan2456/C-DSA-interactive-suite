05976e691ffca4385a7941ff85a6b1ca34beab2e
jagritiuppal158 <jagritiuppal158@gmail.com>
2026-08-01 21:42:00 +0530

    chore(help): remove deprecated and redundant nested help files

--- help/help_advanced_topics.c
+++
@@ -1,6 +0,0 @@
-#include "help.h"
-// This file has been deprecated.
-// - String Algorithms & Compression moved to help_string_compression.c
-// - Benchmarking moved to help_system_utilities.c
-// - DP & Backtracking already in help_dynamic_programming.c / help_backtracking.c
-// Please delete this file using `git rm help_advanced_topics.c`
--- help/help_graphs_trees.c
+++
@@ -1,6 +0,0 @@
-#include "help.h"
-// This file has been split into:
-// - help_trees.c
-// - help_graph_traversals.c
-// - help_advanced_heaps.c
-// Please delete this file using `git rm help_graphs_trees.c`
