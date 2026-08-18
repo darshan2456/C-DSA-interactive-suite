e2375deefa204d3edff9f3e1f4103e2d3e0747dd
Naisarg Purohit <naisargpurohit@gmail.com>
2026-08-18 20:06:07 +0530

    style: format long strings to satisfy clang-format rules

--- help/help_error_correction.c
+++ help/help_error_correction.c
@@ -67,9 +67,9 @@ void help_error_correction_menu(void)
     printf("--- ADLER-32 CHECKSUM ---\n\n");
     printf("ADLER-32 CHECKSUM:\n");
     printf("    A checksum algorithm invented by Mark Adler. It is a modification of the\n");
-    printf("    Fletcher checksum, trading some reliability for software speed compared to CRC.\n\n");
+    printf(\n        "    Fletcher checksum, trading some reliability for software speed compared to CRC.\n\n");
     printf("HOW IT WORKS:\n");
-    printf("    • Computes two 16-bit sums: A (sum of bytes) and B (sum of all individual A values).\n");
+    printf("    • Computes two 16-bit sums: A (sum of bytes) and B (sum of all individual A "\n           "values).\n");
     printf("    • Both sums are calculated modulo 65521 (the largest prime less than 2^16).\n");
     printf("    • The final 32-bit checksum is concatenated as (B << 16) | A.\n\n");
     printf("LIMITATIONS:\n");
